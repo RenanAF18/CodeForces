8f6c28819c0cd58cbb491cbf12bcf096c35d949f
Sávio Renan <renansavio52@gmail.com>
2026-05-14 17:35:44 -0300

    att

---
+++ Codeforces/Problems/cpp/242E.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define rep(i, a, b) for (int i = a; i < (b); i++)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+inline void yes() { cout << "YES\n"; }
+inline void no() { cout << "NO\n"; }
+inline void j1() { cout << "Alice\n"; }
+inline void j2() { cout << "Bob\n"; }
+
+void solve_tc() {}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+
+  int tc = 1;
+  while (tc--)
+    solve_tc();
+
+  return 0;
+}
